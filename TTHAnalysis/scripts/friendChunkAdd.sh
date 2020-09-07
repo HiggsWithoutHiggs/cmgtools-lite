dc29b1c2311da993b76ae9bb89d3ecebbffbee40
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2020-09-07 14:58:57 +0200

    add some old script to manage friends checking / adding

--- TTHAnalysis/scripts/friendChunkAdd.sh
+++ TTHAnalysis/scripts/friendChunkAdd.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-what=$1
-for F in $(ls ${what%.chunk}*.chunk*.root | sed 's/\.chunk[0-9]\+//' | sort | uniq); do
+clean=0
+if [[ "$1" == "-c" ]]; then
+    if test -d Chunks; then
+        echo "Dir Chunks existing. Moving friend chunks there";
+    else
+        echo "Creating Chunks"
+        mkdir /tmp/$USER/Chunks;
+        ln -s /tmp/$USER/Chunks;
+    fi
+    clean=1; shift;
+fi;
+
+dir="./"
+if [[ "$1" != "" ]]; then dir=$1; fi
+for F in $(ls ${dir}/*_Friend.chunk*.root | sed 's/\.chunk[0-9]\+//' | sort | uniq); do
     if test -f $F; then echo "Merged file $F already exists. skipping."; continue; fi
     FILES=$(ls ${F/.root/.chunk*.root} | \
             perl -npe 's/\.chunk(\d+)\./sprintf(".%06d.",$1)/e' | \
@@ -10,3 +23,4 @@ for F in $(ls ${what%.chunk}*.chunk*.root | sed 's/\.chunk[0-9]\+//' | sort | un
     hadd -ff $F $FILES
 done
 
+if [[ $clean == 1 ]]; then mv ${dir}/*_Friend.chunk*.root Chunks; fi
