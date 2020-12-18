cd34f9625860035b4a00b473bbc4721a50c00f13
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2020-12-18 13:46:47 +0100

    cleaning taken from @mdunser

--- TTHAnalysis/python/tools/fastCombinedObjectRecleanerHelper.cxx
+++ TTHAnalysis/python/tools/fastCombinedObjectRecleanerHelper.cxx
@@ -40,11 +40,13 @@ public:
           ruint * unsigned_;
   };
   
-  fastCombinedObjectRecleanerHelper(CollectionSkimmer &clean_taus, CollectionSkimmer &clean_jets, CollectionSkimmer &clean_tjets, CollectionSkimmer &clean_vjets, CollectionSkimmer &clean_ljets, bool cleanJetsWithFOTaus, float bTagL, float bTagM, bool cleanWithRef=false) : clean_taus_(clean_taus), clean_jets_(clean_jets), clean_tjets_(clean_tjets), clean_vjets_(clean_vjets), clean_ljets_(clean_ljets), deltaR2cut(0.16), cleanJetsWithFOTaus_(cleanJetsWithFOTaus), bTagL_(bTagL), bTagM_(bTagM), cleanWithRef_(cleanWithRef), deltaR2cut_taus(0.09), deltaR2cut_fatjets(0.64) {
+  fastCombinedObjectRecleanerHelper(CollectionSkimmer &clean_leps, CollectionSkimmer &clean_taus, CollectionSkimmer &clean_jets, CollectionSkimmer &clean_tjets, CollectionSkimmer &clean_vjets, CollectionSkimmer &clean_fjets, CollectionSkimmer &clean_ljets, bool cleanJetsWithFOTaus, float bTagL, float bTagM, bool cleanWithRef=false) : clean_leps_(clean_leps), clean_taus_(clean_taus), clean_jets_(clean_jets), clean_tjets_(clean_tjets), clean_vjets_(clean_vjets), clean_fjets_(clean_fjets), clean_ljets_(clean_ljets), deltaR2cut(0.16), cleanJetsWithFOTaus_(cleanJetsWithFOTaus), bTagL_(bTagL), bTagM_(bTagM), cleanWithRef_(cleanWithRef), deltaR2cut_taus(0.09), deltaR2cut_fatjets(0.64) {
+    _cl.reset(new std::vector<int>);
     _ct.reset(new std::vector<int>);
     _cj.reset(new std::vector<int>);
     _ctj.reset(new std::vector<int>);
     _cvj.reset(new std::vector<int>);
+    _cfj.reset(new std::vector<int>);
     _clj.reset(new std::vector<int>);
 }
   
@@ -68,8 +70,8 @@ public:
     nJet_ = nJet; Jet_pt_ = jetPt; Jet_eta_ = jetEta; Jet_phi_ = jetPhi; Jet_btagCSV_ = jetbtagCSV; 
     Jet_corr_   = jetpt;
   }
-  void setFatJets(ruint *nJet, rfloats *jetPt, rfloats *jetEta, rfloats *jetPhi, rfloats *jetbtagCSV) {
-    nFJet_ = nJet; FJet_pt_ = jetPt; FJet_eta_ = jetEta; FJet_phi_ = jetPhi; FJet_btagCSV_ = jetbtagCSV; 
+  void setFatJets(ruint *nJet, rfloats *jetPt, rfloats *jetEta, rfloats *jetPhi, rfloats *jetbtagCSV, rfloats *jetttag, rfloats *jetwtag, rfloats *jetztag) {
+    nFJet_ = nJet; FJet_pt_ = jetPt; FJet_eta_ = jetEta; FJet_phi_ = jetPhi; FJet_btagCSV_ = jetbtagCSV; FJet_deepTag_TvsQCD_ = jetttag; FJet_deepTag_WvsQCD_ = jetwtag; FJet_deepTag_ZvsQCD_ = jetztag; 
   }
 
   void addJetPt(int pt){
@@ -177,8 +179,10 @@ public:
   void selectTau(uint i, bool what=true) {sel_taus.get()[i]=what;}
   void selectJet(uint i, bool what=true) {sel_jets.get()[i]=what;}
   void loadTags(CombinedObjectTags *tags, bool cleanTausWithLooseLeptons, float wPL=0, float wPM=0){
+    //std::cout << __LINE__ << " in loadtags" << std::endl;
     bTagL_ = wPL; bTagM_ = wPM;
-    std::copy(tags->lepsC.get(),tags->lepsC.get()+*nLep_,sel_leps.get());
+    //std::copy(tags->lepsC.get(),tags->lepsC.get()+*nLep_,sel_leps.get()); //marc
+    std::copy(tags->lepsT.get(),tags->lepsT.get()+*nLep_,sel_leps.get());
     if (cleanTausWithLooseLeptons) std::copy(tags->lepsL.get(),tags->lepsL.get()+*nLep_,sel_leps_extrafortau.get());
     std::copy(tags->tausF.get(),tags->tausF.get()+*nTau_,sel_taus.get());
     std::copy(tags->jetsS.get(),tags->jetsS.get()+*nJet_,sel_jets.get());
@@ -188,127 +192,178 @@ public:
   void setDRFatJets(float f) {deltaR2cut_fatjets = f*f;}
 
   std::vector<std::vector<int>* > run() {
+
+//std::cout<< "=================="<<std::endl;
     clean_taus_.clear();
+    clean_leps_.clear();
     clean_jets_.clear();
     clean_tjets_.clear();
     clean_vjets_.clear();
+    clean_fjets_.clear();
     clean_ljets_.clear();
 
+//std::cout<<__LINE__<<std::endl;
+    _cl->clear();
     _ct->clear();
     _cj->clear();
     _ctj->clear();
     _cvj->clear();
+    _cfj->clear();
     _clj->clear();
 
-    for (int iT = 0, nT = *nTau_; iT < nT; ++iT) {
-      if (!sel_taus[iT]) continue;
-      bool ok = true;
-      for (int iL = 0, nL = *nLep_; iL < nL; ++iL) {
-	if (!(sel_leps.get()[iL] || sel_leps_extrafortau.get()[iL])) continue;
-	if (deltaR2((*Lep_eta_)[iL], (*Lep_phi_)[iL], (*Tau_eta_)[iT], (*Tau_phi_)[iT]) < deltaR2cut_taus) {
-	  ok = false;
-	  break;
-	}
+    std::vector<int> other_fatjets;
+    std::vector<int> bad_leptons;
+    bad_leptons.clear();
+
+//std::cout<< "total number of leptons " << *nLep_ << std::endl;
+//std::cout<< "total number of fat jets " << *nFJet_ << std::endl;
+// beginning marc
+
+    std::vector<float> vetos_eta;
+    std::vector<float> vetos_phi;
+    std::vector<int>   vetos_indices;
+    for (int iL = 0, nL = *nLep_; iL < nL; ++iL) if (sel_leps[iL]) {vetos_eta.push_back((*Lep_eta_)[iL]); vetos_phi.push_back((*Lep_phi_)[iL]); if (Lep_jet_) vetos_indices.push_back((*Lep_jet_)[iL]);}
+    for (int iT = 0, nT = *nTau_; iT < nT; ++iT) if (sel_taus[iT]) {vetos_eta.push_back((*Tau_eta_)[iT]); vetos_phi.push_back((*Tau_phi_)[iT]); if (Tau_jet_) vetos_indices.push_back((*Tau_jet_)[iT]);}
+
+    std::unique_ptr<bool[]> good;   good.reset(new bool[*nJet_]);    std::fill_n(good.get(),*nJet_,true);
+    std::unique_ptr<bool[]> goodfj; goodfj.reset(new bool[*nFJet_]); std::fill_n(goodfj.get(),*nFJet_,true);
+
+    for (int iFJ = 0, nFJ = *nFJet_; iFJ < nFJ; ++iFJ) {
+      float tagger_t = (*FJet_deepTag_TvsQCD_)[iFJ];
+      if (tagger_t>0.436) { // these are the good top fatjets, WP iwth 2.5% mistag rate
+        clean_tjets_.push_back(iFJ);
+        _ctj->push_back(iFJ);
+
+        // now clean the leptons from these good fat topjets
+
+        //  mark here the ones that aren't good!
+        for (int iL = 0, nL = *nLep_; iL < nL; ++iL) {
+          if (!(sel_leps.get()[iL])) { // || sel_leps_extrafortau.get()[iL])) { 
+            bad_leptons.push_back(iL); 
+            continue; // not sure that second statement after the or is...
+          }
+          if (reco::deltaR2((*Lep_eta_)[iL], (*Lep_phi_)[iL], (*FJet_eta_)[iFJ], (*FJet_phi_)[iFJ]) < deltaR2cut_fatjets) {
+            bad_leptons.push_back(iL);
+            //std::cout << "found a bad lepton with index " << iL << std::endl;
+            // clean_leps_.push_back(iL);
+            // _cl.push_back(iL);
+          }
+        }
+      } 
+      else {
+        other_fatjets.push_back(iFJ);
       }
-      if (ok) {
-	clean_taus_.push_back(iT);
-	_ct->push_back(iT);
-      } else {
-	sel_taus.get()[iT]=false; // do not use unclean taus for cleaning jets, use lepton instead
+
+    } // cleaned the top fat jets from the leptons
+
+
+//std::cout<<__LINE__<<std::endl;
+    //std::cout << " this is the size of bad_leptons " << bad_leptons.size() << std::endl;
+    //std::cout << " this is the size of tp jets " << _ctj->size() << std::endl;
+
+    // put all clean leptons into a vector ...
+    for (int iL = 0, nL = *nLep_; iL < nL; ++iL) {
+
+      bool lepIsGood = true;
+      if (!(sel_leps.get()[iL] )) lepIsGood = false; //|| sel_leps_extrafortau.get()[iL])) continue; // not sure that second statement after the or is...
+
+      for (std::vector<int>::iterator iBL = bad_leptons.begin(); iBL != bad_leptons.end(); ++iBL) {
+        if (*iBL == iL) lepIsGood = false;
+      }
+
+      if (lepIsGood) {_cl->push_back(iL); clean_leps_.push_back(iL);}
+    }
+
+    //std::cout << " this is the size of good_leptons " << _cl->size() << std::endl;
+
+//std::cout<<__LINE__<<std::endl;
+
+    
+    // loop on the rest of the fatjets
+    for (std::vector<int>::iterator iFJ = other_fatjets.begin(); iFJ != other_fatjets.end(); iFJ++){
+      float tagger_w = (*FJet_deepTag_WvsQCD_)[*iFJ];
+      float tagger_z = (*FJet_deepTag_ZvsQCD_)[*iFJ];
+
+      bool jetIsGood = true;
+
+      for(std::vector<int>::iterator iL = _cl->begin(); iL != _cl->end(); ++iL) {
+        float tmp_dr =  reco::deltaR2((*Lep_eta_)[*iL], (*Lep_phi_)[*iL], (*FJet_eta_)[*iFJ], (*FJet_phi_)[*iFJ]);
+
+        if (tmp_dr < deltaR2cut_fatjets) jetIsGood = false;
+      }
+
+      if (!jetIsGood) continue;
+
+      if( (tagger_w > 0.762 || tagger_z > 0.479) ) {
+        clean_vjets_.push_back(*iFJ); // tag-values for 2.5% mistag
+        _cvj->push_back(*iFJ); // tag-values for 2.5% mistag
+      }
+      else {
+        clean_fjets_.push_back(*iFJ);
+        _cfj->push_back(*iFJ);
       }
     }
 
-    { // jet cleaning (clean closest jet - one at most - for each lepton or tau, then apply jet selection)
-      std::vector<float> vetos_eta;
-      std::vector<float> vetos_phi;
-      std::vector<int>   vetos_indices;
-      for (int iL = 0, nL = *nLep_; iL < nL; ++iL) if (sel_leps[iL]) {vetos_eta.push_back((*Lep_eta_)[iL]); vetos_phi.push_back((*Lep_phi_)[iL]); if (Lep_jet_) vetos_indices.push_back((*Lep_jet_)[iL]);}
-      for (int iT = 0, nT = *nTau_; iT < nT; ++iT) if (sel_taus[iT]) {vetos_eta.push_back((*Tau_eta_)[iT]); vetos_phi.push_back((*Tau_phi_)[iT]); if (Tau_jet_) vetos_indices.push_back((*Tau_jet_)[iT]);}
-      std::unique_ptr<bool[]> good;   good.reset(new bool[*nJet_]);    std::fill_n(good.get(),*nJet_,true);
-      std::unique_ptr<bool[]> goodfj; goodfj.reset(new bool[*nFJet_]); std::fill_n(goodfj.get(),*nFJet_,true);
-      if (cleanWithRef_){ // only for std jets
-	for (uint iV=0; iV<vetos_indices.size(); iV++) {
-	  if (vetos_indices[iV] > -1) good[vetos_indices[iV]] = false;
-	}
-      }
-      else{
-	for (uint iV=0; iV<vetos_eta.size(); iV++) {
-	  float mindr2 = -1; int best = -1;
-	  for (int iJ = 0, nJ = *nJet_; iJ < nJ; ++iJ) {
-	    float dr2 = deltaR2(vetos_eta[iV],vetos_phi[iV],(*Jet_eta_)[iJ], (*Jet_phi_)[iJ]);
-	    if (mindr2<0 || dr2<mindr2) {mindr2=dr2; best=iJ;}
-	  }
-	  if (best>-1 && mindr2<deltaR2cut) {
-	    good[best] = false;
-	  }
-	  mindr2 = -1; best = -1;
-	  for (int iFJ = 0, nFJ = *nFJet_; iFJ < nFJ; ++iFJ) {
-	    float dr2 = deltaR2(vetos_eta[iV],vetos_phi[iV],(*FJet_eta_)[iFJ], (*FJet_phi_)[iFJ]);
-	    if (mindr2<0 || dr2<mindr2) {mindr2=dr2; best=iFJ;}
-	  }
-	  if (best>-1 && mindr2<deltaR2cut) {
-	    goodfj[best] = false;
-	  }
-	}
-      }
-      for (int iJ = 0, nJ = *nJet_; iJ < nJ; ++iJ) {
-	if (good[iJ] && sel_jets[iJ]) {
-	  _cj->push_back(iJ);
-	  if(fabs((*Jet_eta_)[iJ]) < 2.4)
-	    clean_jets_.push_back(iJ); // only to count fwd jets
-          bool lightjet = true;
-          for (int iFJ = 0, nFJ = *nFJet_; iFJ < nFJ; ++iFJ) { // clean all the jets mathcing a fatjet to count as a light jet
-            float dr2 = deltaR2((*Jet_eta_)[iJ],(*Jet_phi_)[iJ],(*FJet_eta_)[iFJ],(*FJet_phi_)[iFJ]);
-            if (goodfj[iFJ] && dr2<deltaR2cut_fatjets) 
-              lightjet = false;
-          }
-          if (lightjet) {
-            _clj->push_back(iJ);
-            clean_ljets_.push_back(iJ);
-          }
-	}
-      }
-      for (int iFJ = 0, nFJ = *nFJet_; iFJ < nFJ; ++iFJ) {
-        if (goodfj[iFJ]) {
-          float csv = (*FJet_btagCSV_)[iFJ];
-          if (csv>bTagM_) {
-            _ctj->push_back(iFJ);
-            clean_tjets_.push_back(iFJ);
-          } 
-          else {
-            _cvj->push_back(iFJ);
-            clean_vjets_.push_back(iFJ);            
-          }
+    for (int iJ = 0, nJ = *nJet_; iJ < nJ; ++iJ) {
+      if ( good[iJ] && sel_jets[iJ]) {
+        _cj->push_back(iJ);
+        if(fabs((*Jet_eta_)[iJ]) < 2.4) clean_jets_.push_back(iJ); // only to count fwd jets. marc: i don't get it...
+        bool lightjet = true;
+
+        for (int iFJ = 0, nFJ = *nFJet_; iFJ < nFJ; ++iFJ) { // clean all the jets mathcing a fatjet to count as a light jet
+          float dr2 = reco::deltaR2((*Jet_eta_)[iJ],(*Jet_phi_)[iJ],(*FJet_eta_)[iFJ],(*FJet_phi_)[iFJ]);
+          if (goodfj[iFJ] && dr2<deltaR2cut_fatjets)  
+            lightjet = false;
+        }
+
+        for(std::vector<int>::iterator iL = _cl->begin(); iL != _cl->end(); ++iL) { // clean all the regular jets with the leptons
+          float dr2 = reco::deltaR2((*Jet_eta_)[iJ],(*Jet_phi_)[iJ],(*Lep_eta_)[*iL],(*Lep_phi_)[*iL]);
+          if (dr2 < deltaR2cut)  lightjet = false;
+        }
+
+        if (lightjet ) {
+          _clj->push_back(iJ);
+          clean_ljets_.push_back(iJ);
         }
       }
     }
+
+//std::cout<<__LINE__<<std::endl;
     std::vector<std::vector<int>* > ret;
-    ret.push_back(_ct.get());
-    ret.push_back(_cj.get());
+    ret.push_back(_cl .get());
+    ret.push_back(_ct .get());
+    ret.push_back(_cj .get());
     ret.push_back(_ctj.get());
     ret.push_back(_cvj.get());
+    ret.push_back(_cfj.get());
     ret.push_back(_clj.get());
+//std::cout<<__LINE__<<std::endl;
     return ret;
   }
 
+// end marc
+
+
 private:
   std::unique_ptr<bool[]> sel_leps, sel_leps_extrafortau, sel_taus, sel_jets;
-  CollectionSkimmer &clean_taus_, &clean_jets_, &clean_tjets_, &clean_vjets_, &clean_ljets_;
+  CollectionSkimmer &clean_leps_, &clean_taus_, &clean_jets_, &clean_tjets_, &clean_vjets_, &clean_fjets_, &clean_ljets_;
   rcount nLep_, nTau_, nJet_, nFJet_, nLJet_;
   rfloats *Lep_pt_, *Lep_eta_, *Lep_phi_;
   rfloats *Tau_pt_, *Tau_eta_, *Tau_phi_;
   rfloats *Jet_pt_, *Jet_phi_, *Jet_eta_, *Jet_btagCSV_;
-  rfloats *FJet_pt_, *FJet_phi_, *FJet_eta_, *FJet_btagCSV_;
+  rfloats *FJet_pt_, *FJet_phi_, *FJet_eta_, *FJet_btagCSV_, *FJet_deepTag_TvsQCD_, *FJet_deepTag_WvsQCD_, *FJet_deepTag_ZvsQCD_;
   vector<rfloats*> Jet_corr_;
   rints    *Lep_jet_, *Tau_jet_;
   float deltaR2cut;
   float deltaR2cut_taus;
   float deltaR2cut_fatjets;
   std::set<int> _jetptcuts;
+  std::unique_ptr<std::vector<int> > _cl;
   std::unique_ptr<std::vector<int> > _ct;
   std::unique_ptr<std::vector<int> > _cj;
   std::unique_ptr<std::vector<int> > _ctj;
   std::unique_ptr<std::vector<int> > _cvj;
+  std::unique_ptr<std::vector<int> > _cfj;
   std::unique_ptr<std::vector<int> > _clj;
   bool cleanJetsWithFOTaus_;
   float bTagL_,bTagM_;
